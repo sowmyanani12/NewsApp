4cec237e32aa8a8afd0d3c6e26ead4c1eb3c162e
Sowmya Jorka <sowmyanani12@gmail.com>
2022-12-14 21:15:31 +0500

    Splash ACtivity
    new activity added

--- app/src/main/java/com/project/newsapp/activities/SplashActivity.java
+++ app/src/main/java/com/project/newsapp/activities/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.project.newsapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.project.newsapp.R;
 
@@ -12,5 +14,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> startActivity(new Intent(SplashActivity.this, MainActivity.class)), 3000);
+
     }
 }
